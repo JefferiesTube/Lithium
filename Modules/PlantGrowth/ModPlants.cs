7e16bef0ed438599c66ea3579a2151f3e665d738
Thomas Hummes <thomas.hummes@googlemail.com>
2025-08-12 22:15:40 +0200

    Merged PR from ifBars: Fix 0/0 buds plant issue

--- Modules/PlantGrowth/ModPlants.cs
+++ Modules/PlantGrowth/ModPlants.cs
@@ -25,9 +25,13 @@ namespace Lithium.Modules.PlantGrowth
         public float GrowthModifier = 1f;
         public float WaterDrainModifier = 1f;
 
-        public List<WeightedFloat> RandomYieldsPerBudModifier = [ new(7.0f, 1.0f), new(2.0f, 2.0f), new(1.0f, 3.0f) ];
-        public List<WeightedFloat> RandomYieldModifiers = [ new(7.5f, 1), new(1.0f, 0.25f), new(1.0f, 1.5f), new(0.5f, 3.0f)];
-        public List<WeightedFloat> RandomQualityModifiers = [new(0, -0.5f), new(0.5f, 0), new(0.75f, 0), new(0.2f, 0.4f), new(0.01f, 0.5f)];
+        public static readonly List<WeightedFloat> RandomYieldsPerBudModifierDefaults = [ new(7.0f, 1.0f), new(2.0f, 2.0f), new(1.0f, 3.0f) ];
+        public static readonly List<WeightedFloat> RandomYieldModifiersDefaults = [ new(7.5f, 1), new(1.0f, 0.25f), new(1.0f, 1.5f), new(0.5f, 3.0f)];
+        public static readonly List<WeightedFloat> RandomQualityModifiersDefaults = [new(0, -0.5f), new(0.5f, 0), new(0.75f, 0), new(0.2f, 0.4f), new(0.01f, 0.5f)];
+
+        public List<WeightedFloat> RandomYieldsPerBudModifier = RandomYieldsPerBudModifierDefaults;
+        public List<WeightedFloat> RandomYieldModifiers = RandomYieldModifiersDefaults;
+        public List<WeightedFloat> RandomQualityModifiers = RandomQualityModifiersDefaults;
 
         [JsonIgnore] public WeightedPicker<float> RandomYieldPerBudPicker;
         [JsonIgnore] public WeightedNormalizer RandomYieldModifierPicker;
@@ -55,21 +59,36 @@ namespace Lithium.Modules.PlantGrowth
         {
             base.Load();
             Configuration.RandomYieldPerBudPicker = new();
-            foreach (WeightedFloat entry in Configuration.RandomYieldsPerBudModifier)
-            {
-                Configuration.RandomYieldPerBudPicker.Add(entry.Value, entry.Weight);
-            }
+
+            Configuration.RandomYieldPerBudPicker.AddRange(Configuration.RandomYieldPerBudPicker.Count == 0 
+                ? ModPlantsConfiguration.RandomYieldsPerBudModifierDefaults.Select(p => new KeyValuePair<float, float>(p.Value, p.Weight))
+                : Configuration.RandomYieldsPerBudModifier.Select(p => new KeyValuePair<float, float>(p.Value, p.Weight)));
+
 
             Configuration.RandomYieldModifierPicker = new();
             foreach (WeightedFloat entry in Configuration.RandomYieldModifiers)
             {
                 Configuration.RandomYieldModifierPicker.Add(entry.Weight, entry.Value);
             }
+            if (Configuration.RandomYieldModifiers.Count == 0 || Configuration.RandomYieldModifiers.Sum(e => e.Weight) <= 0f)
+            {
+                foreach (WeightedFloat value in ModPlantsConfiguration.RandomYieldModifiersDefaults)
+                {
+                    Configuration.RandomYieldModifiers.Add(value);
+                }
+            }
 
             Configuration.RandomYieldQualityPicker = new();
             foreach (WeightedFloat entry in Configuration.RandomQualityModifiers)
             {
                 Configuration.RandomYieldQualityPicker.Add(entry.Weight, entry.Value);
+            }
+            if (Configuration.RandomQualityModifiers.Count == 0 || Configuration.RandomQualityModifiers.Sum(e => e.Weight) <= 0f)
+            {
+                foreach (WeightedFloat value in ModPlantsConfiguration.RandomQualityModifiersDefaults)
+                {
+                    Configuration.RandomYieldQualityPicker.Add(value.Weight, value.Value);
+                }
             }
         }
 
